0085c3e015dc960d11358fa4bc7677e8b36b3e40
GoodSilver9 <hool784@naver.com>
2023-11-30 22:47:06 +0900

    ⚡️ feature: search.js 에 import 구문 추가

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -1,20 +1,24 @@
+import fetch from 'node-fetch';
+
 export async function handler(event, context) {
-	const query = event.queryStringParameters.query;
-	console.log(query);
-	const url = `https://openapi.naver.com/v1/search/news.json?query=${encodeURIComponent(
-		query
-	)}`;
-	const response = await fetch(url, {
-		headers: {
-			"X-Naver-Client-Id": process.env.REACT_APP_CLIENT_ID,
-			"X-Naver-Client-Secret": process.env.REACT_APP_CLIENT_SECRET,
-		},
-	});
-	console.log(response);
-	const data = await response.json();
-	console.log(data);
-	return {
-		statusCode: 200,
-		body: JSON.stringify(data),
-	};
+  const query = event.queryStringParameters.query;
+  console.log(query);
+  const url = `https://openapi.naver.com/v1/search/news.json?query=${encodeURIComponent(
+    query,
+  )}`;
+  console.log(url);
+
+  const response = await fetch(url, {
+    headers: {
+      'X-Naver-Client-Id': process.env.REACT_APP_CLIENT_ID,
+      'X-Naver-Client-Secret': process.env.REACT_APP_CLIENT_SECRET,
+    },
+  });
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
 }
