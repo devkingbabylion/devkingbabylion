dcf7e226d94a2fc3c2c3dd7eb39794cc1622dd38
GoodSilver9 <hool784@naver.com>
2023-12-08 23:21:53 +0900

    ✨ Feature: 카드컴포넌트 스타일파일화

--- src/Components/Card/ProgrammingCard.jsx
+++ src/Components/Card/ProgrammingCard.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import {
+  StyledProgrammingCardLayOut,
+  StyledHoverCardLayOut,
+} from './programmingCard.styled';
 import linkIcon from '../../Assets/link.svg';
 import Angular from '../../Assets/Angular.svg';
 import JavaScript from '../../Assets/JavaScript.svg';
@@ -11,63 +14,6 @@ import Sass from '../../Assets/Sass.svg';
 import TailWind from '../../Assets/Tailwind.svg';
 import TypeScript from '../../Assets/TypeScript.svg';
 import Vue from '../../Assets/Vue.svg';
-
-const StyledProgrammingCard = styled.div`
-  width: 347px;
-  height: 310px;
-  border-radius: 20px;
-  background: linear-gradient(
-    to bottom,
-    #a3a3a3 0%,
-    #a3a3a3 60%,
-    rgba(0, 0, 0, 0.2) 60%,
-    rgba(0, 0, 0, 0.2) 100%
-  );
-
-  .item {
-    display: flex;
-    align-items: center;
-    padding-top: 40%;
-    color: #fff;
-    font-size: 30px;
-  }
-
-  img {
-    width: 100px;
-    height: 100px;
-  }
-`;
-const StyledHoverCard = styled.div`
-  width: 100%;
-  height: 100%;
-  border-radius: 20px;
-  background-color: #8a8a8a;
-  color: #fff;
-  display: flex;
-  flex-direction: column;
-  gap: 19px;
-  img {
-    width: 18px;
-    height: 18px;
-  }
-  h1 {
-    font-size: 30px;
-    margin-top: 23px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-  div {
-    display: flex;
-    align-items: center;
-    margin-left: 27px;
-    color: #fff;
-  }
-  a {
-    margin-left: 8px;
-    color: #fff;
-    text-decoration: underline #fff;
-  }
-`;
 
 export default function ProgrammingCard({ name, links }) {
   const [card, setCard] = useState(false);
@@ -86,7 +32,7 @@ export default function ProgrammingCard({ name, links }) {
 
   return (
     <div>
-      <StyledProgrammingCard
+      <StyledProgrammingCardLayOut
         onMouseEnter={() => setCard(true)}
         onMouseLeave={() => setCard(false)}
       >
@@ -97,7 +43,7 @@ export default function ProgrammingCard({ name, links }) {
           </div>
         )}
         {card && (
-          <StyledHoverCard>
+          <StyledHoverCardLayOut>
             <h1>{name}</h1>
             {links &&
               links.map((link, index) => (
@@ -106,9 +52,9 @@ export default function ProgrammingCard({ name, links }) {
                   <a href={link.url}>{link.title}</a>
                 </div>
               ))}
-          </StyledHoverCard>
+          </StyledHoverCardLayOut>
         )}
-      </StyledProgrammingCard>
+      </StyledProgrammingCardLayOut>
     </div>
   );
 }
