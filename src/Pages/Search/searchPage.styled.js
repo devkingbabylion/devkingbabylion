51ba4686d60e3090a47626d4e60630e913f63576
GoodSilver9 <hool784@naver.com>
2023-12-09 13:24:51 +0900

    ✨ Feature: Search페이지 카드컴포넌트 정렬

---
+++ src/Pages/Search/searchPage.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SearchResultLayOut = styled.ul`
+  display: flex;
+  width: 1200px;
+  margin: 92px 0px 159px 200px;
+  gap: 61px 85px;
+  flex-wrap: wrap;
+`;
