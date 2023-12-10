c40e12251994a536c5ca7031a947303899446b22
esoby <141106@naver.com>
2023-12-10 14:15:43 +0900

    ✨ feat : api 연결 제거

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,31 +6,20 @@ import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import devkingbabylion from '../../Assets/devkingbabylion.png';
 import SearchBar from '../SearchBar/SearchBar';
+
 export default function Header({ type }) {
   const [query, setQuery] = useState('');
   const [error, setError] = useState(null);
   const setSearchResult = useSetRecoilState(searchResultState);
   const navigate = useNavigate();
 
-  const handleSearch = async () => {
-    const encText = encodeURIComponent(query); // 올바른 URL 인코딩을 위해 변수에서 함수로 수정
-    const url = `/.netlify/functions/search?query=${encText}`;
-
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('네이버 API 요청 실패');
-      }
-
-      const data = await response.json();
-      setSearchResult(data);
-      setError(null);
-
-      navigate('/search');
-    } catch (error) {
-      console.error('Error:', error);
-      setError('API 요청 중 오류가 발생했습니다.');
+  const handleSearch = () => {
+    if (!query.trim()) {
+      setError('검색어를 입력해 주세요');
+      return;
     }
+    setSearchResult({ query });
+    navigate('/search');
   };
 
   switch (type) {
