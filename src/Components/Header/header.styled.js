d44e82363ad231c133af68aad1b909b4cafd184a
jiwanchoi <wldhks303@naver.com>
2023-12-13 18:02:29 +0900

    :recycle: Refactor: header반응형
    header반응형

--- src/Components/Header/header.styled.js
+++ src/Components/Header/header.styled.js
@@ -12,10 +12,24 @@ export const StyledHeaderLayout = styled.header`
   font-size: 18px;
   border-bottom: 1px solid #bdbdbd;
   width: 100%;
-  position: fixed;
+  position: relative;
+  // position: fixed;
   top: 0;
   transition: transform 0.5s ease;
-
+  min-width: 414px;
+  .home .icon {
+    display: none;
+  }
+  @media (max-width: 580px) {
+    .home .text {
+      display: none;
+    }
+    .home .icon {
+      display: inline-block;
+      width: 40px;
+      height: 40px;
+    }
+  }
   ${({ scrolling }) =>
     scrolling &&
     css`
